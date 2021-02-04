b6c108ab8d743e89bc164b621cf8a7f215abdad9
An Huynh <anh005@ucsd.edu>
2021-02-03 20:27:37 -0800

    Up

--- routes/add.js
+++ routes/add.js
@@ -2,18 +2,11 @@ var data = require("../data.json");
 
 exports.addFriend = function(request, response) {  
 	// Your code goes here
-	var name = request.query.name;
-	var description = request.query.description;
-	var image = request.query.image;
-	console.log(name + description);
-
-	response.render('index', {
-		'name:': 'name',
-		'description:': 'description',
-		'imageURL': 'http://lorempixel.com/400/400/people'
-	});
-}
-
-function addFriend(name, description) {
-	return 
-}
+	response.render('index', data);
+	var newFriend = {
+		"name": request.query.name, 
+		"description": request.query.description,
+		"imageURL": "http://lorempixel.com/400/400/people"
+	};
+	data.friends.push(newFriend);
+};
